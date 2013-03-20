6a4f112073cb856bd4247cca90ba7fd071439c77
Cristian Pereyra <criis.pereyra@gmail.com>
2013-03-20 19:29:09 -0300

    adds some tests for rotations, removes all kind of rule evaluations

---
+++ src/test/java/ar/edu/itba/sia/gps/impl/PieceTest.java
@@ -0,0 +1,12 @@
+package ar.edu.itba.sia.gps.impl;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cris
+ * Date: 20/03/13
+ * Time: 19:02
+ */
+public class PieceTest {
+//    @Test
+//    public void
+}
