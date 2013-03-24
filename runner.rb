6ec060d25033c0f3b5316fcf72848de71cd40cc0
Cristian Pereyra <criis.pereyra@gmail.com>
2013-03-24 20:27:27 -0300

    adds 6x6 game

---
+++ runner.rb
@@ -0,0 +1,6 @@
+heuristics = %w(center color order order,color)
+costfunction = %w(rotation dummy center)
+noninformed = %w(DFS BFS ID)
+files = %w(random.2.xml random.3.xml random.4.xml random.5.xml random.6.xml)
+informed = %w(AStar greedy)
+
--- src/main/java/ar/edu/itba/sia/domain/persist/generate/GameGenerator.java
+++ src/main/java/ar/edu/itba/sia/domain/persist/generate/GameGenerator.java
@@ -12,7 +12,7 @@ public class GameGenerator {
     private float[] probabilities;
 
     public static void main(final String[] args) throws Exception {
-		GameGenerator generator = new GameGenerator(4, 6);
+		GameGenerator generator = new GameGenerator(6, 6);
         GameXML game = generator.generate();
         game.toFile("random.xml");
 	}
