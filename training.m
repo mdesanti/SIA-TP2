1a36a528ddf7b127391d28e89426683ef14e0523
Cristian Pereyra <criis.pereyra@gmail.com>
2013-04-10 23:19:16 -0300

    fixes bug and adds vagrantfile

--- training.m
+++ training.m
@@ -15,8 +15,6 @@ function y = generateTrainingSets(n)
 	for i=0:max-1
 		out = (zeros(1,n));
 		out = binary2vector(i,n);
-%		out = out .- 0.5;
-%		out = out .* 2;
 		x(k,1) = -1;
 		x(k,2:length(out)+1) = out;
 		k++;
@@ -27,32 +25,29 @@ end
 function x = train(n)
 	weights = (2*rand(1,n+1)-1)/2;
 	sets = generateTrainingSets(n)
-	finished = zeros(1,2^n);
+	max = 2^n
+	finished = zeros(1,max);
 	i = 1;
-	while(sum(finished) <= 8 && i < 100*(2^n))
-		num = mod(i,2^n) + 1;
-		result = stepNeuron(weights, sets(num,:));
-		should = prod(sets(num, 2:n));
+	while(prod(finished) < 1 && i < 10000*(max))
+		num = mod(i,max) + 1;
+		in = sets(num,:);
+		inWithNoBias = in(2:n+1);
+		result = stepNeuron(weights, in);
+		should = prod(inWithNoBias);
 		if(result != should)
-			weights = weights .+ fixWeights(sets(num,:), should, result);
-%			disp(fixWeights(sets(num,:), should, result));
-			finished = zeros(1,2^n);
+			weights = weights .+ fixWeights(in, should, result);
+			finished = zeros(1,max);
 		else
 			finished(num) += 1;
 		end
 		fflush(stdout);
 		i++;
 	end
-	finished
-
-	for	i=1:2^n
-		work(weights, sets(i,2:2^n -1))
-	end
 	x = weights;
 end
 
 function out = fixWeights(in, should, was)
-	eta = 0.15;
+	eta = 0.015;
 	out =in .* eta*(should-was);
 end
 
