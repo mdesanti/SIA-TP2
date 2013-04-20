3d7de59d34e0e5b9da97f1105ea1650b9d289210
Matias De Santi <mdesanti90@gmail.com>
2013-04-20 19:28:13 -0300

    Added results and screenshots

--- run.m
+++ run.m
@@ -2,7 +2,7 @@ clear
 clc
 main
 network.problem = problem.approximation(2, functs.tanh)
-network.trainPctg = 0.8
+network.trainPctg = 0.2
 network.beta = 0.5
 %sinData = sin([-499:500]);
 %network.data = sinData;
