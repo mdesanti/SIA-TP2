254b91ffdcce353fb52334e71a53568cb6a8ad6f
Esteban Pintos <egpintos17@hotmail.com>
2013-04-20 20:39:43 -0300

    added new results with 3

--- run.m
+++ run.m
@@ -1,10 +1,10 @@
 clear
 clc
 main
-network.problem = problem.approximation(2, functs.tanh)
-network.trainPctg = 0.2
+network.problem = problem.approximation(3, functs.tanh)
+network.trainPctg = 0.6
 network.beta = 0.5
 %sinData = sin([-499:500]);
 %network.data = sinData;
 network.neuronsPerLayer = [20 1];
-network.train(2)
+network.train(3)
