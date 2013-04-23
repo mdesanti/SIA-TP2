a5c826757c8308b7abaef8a8bb96b1d69f091755
Cristian Pereyra <criis.pereyra@gmail.com>
2013-04-23 19:31:17 -0300

    sets adaptive eta with a different startValue

--- run.m
+++ run.m
@@ -3,12 +3,16 @@ clc
 main
 network.n = 3;
 network.problem = problem.approximation(network.n, functs.tanh);
-network.startEta = 0.005;
-network.iterLimit = 200;
+network.iterLimit = 500;
 network.trainPctg = 0.4;
 network.momentum = true;
-network.adaptive = true;
+network.adaptive = false;
 network.beta = 0.5;
+if network.adaptive
+	network.startEta = 1;
+else
+	network.startEta = 0.01;
+end
 network.neuronsPerLayer = [20 1];
 network.train(network.n);
 save([logging.pwd,'/','network'],'network');
