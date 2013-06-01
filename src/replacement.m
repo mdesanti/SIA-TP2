38f9122208fb967972fde3bda6d2398724262817
Matias De Santi <mdesanti90@gmail.com>
2013-06-01 16:33:14 -0300

    Added replacement method 3

--- src/replacement.m
+++ src/replacement.m
@@ -1,13 +1,14 @@
 function r = replacement()
     r.r1 = @rmethods1;
     r.r2 = @rmethods2;
+    r.r3 = @rmethods3;
 end
 
 function x = rmethod2(items, N)
     iterations = N/2;
 end
 
-function x = rmethod1(networks, evaluations)
+function x = rmethods1(networks, evaluations)
     global genetic;
     selection = [];
     nextGeneration = networks;
@@ -24,3 +25,49 @@ function x = rmethod1(networks, evaluations)
     end
     x = nextGeneration;
 end
+
+function x = rmethods3(networks, evaluations)
+    global genetic;
+    selection = [];
+    nextGeneration = networks;
+    N = ceil(length(networks)/2);
+    for i=1:genetic.k
+       selection = genetic.firstSelectionMethod(evaluations, 2)
+       winner1 = networks(selection(1));
+       winner2 = networks(selection(2));
+       [child1 child2] = genetic.crossoverMethod(winner1, winner2);
+       nextGeneration(i) = genetic.mutate(child1);
+    end
+    nexgGenerationEvaluations = [];
+    for i=1:length(nextGeneration)
+        for i=from+1:1000-2
+            result = [];
+            aux = network.eval(network.testSet(i,2:3))*3.8;
+            result(i-from) = aux - network.problem.expected(i)*3.8;
+        end
+        nextGenerationEvaluations(i) = 1/sum(result.^2)/length(result);
+    end
+    len = length(evaluations);
+    newLen = length(nextGenerationEvaluations);
+    evaluations(len:len+newLen-1) = nextGenerationEvaluations(1:newLen);
+    selection = genetic.secondSelectionMethod(evaluations, length(networks));
+    
+    netLen = length(networks);
+    for i=1:length(selection)
+        net;
+        if (selection(i) <= netLen)
+            net = networks(i);
+        else
+            net = nextGeneration(i);
+        end
+    end
+    
+    x;
+end
+
+
+
+
+
+
+
